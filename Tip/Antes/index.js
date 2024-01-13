8d3492f39a825e9718df6b6b420306b0c1deb259
vialmeeida <vitoriaalmeeida11@gmail.com>
2024-01-13 17:46:20 +0000

    Project Tip
    Part 2

--- Tip/Antes/index.js
+++ Tip/Antes/index.js
@@ -1,4 +1,11 @@
-document.getElementById('botao').onclick = tipCalculator;
+
+document.getElementById('botao').onclick = calcula;
+
+function calcula() {
+    const calculo = tipCalculator();
+    console.log(calculo);
+    splitTip(calculo);
+}
 
 function tipCalculator() {
     const tipPercentage = Number(document.getElementById('tipInput').value);
@@ -9,10 +16,25 @@ function tipCalculator() {
 
     const totalTip = tipPercentage / 100 * bill;
 
+    const total = totalTip + bill;
+
     document.getElementById('tipValue').innerHTML = totalTip.toFixed(2);
 
-    document.getElementById('totalWithTip').innerHTML = (totalTip + bill).toFixed(2);
-
+    document.getElementById('totalWithTip').innerHTML = total.toFixed(2);
 
     console.log(totalTip);
+
+    return total;
 }
+
+function splitTip(totalBill) {
+    const numberPeople = Number(document.getElementById('splitInput').value);
+    
+    const billAfterSplit = totalBill / numberPeople;
+
+    console.log(billAfterSplit);
+
+    document.getElementById('billEach').innerHTML = billAfterSplit.toFixed(2);
+}
+
+
