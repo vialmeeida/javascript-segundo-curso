51d5356aa45007a2f6cac161b6f06fc0ca326f97
vialmeeida <vitoriaalmeeida11@gmail.com>
2024-02-17 13:41:42 +0000

    Project Guess the number final

--- Guess the Number/Antes/index.js
+++ Guess the Number/Antes/index.js
@@ -1,6 +1,5 @@
-//Pegando um numero randomico
-const randomNumber = Math.floor(Math.random() * 100);
 
+const randomNumber = Math.floor(Math.random() * 100 + 1);
 console.log(randomNumber);
 
 //pegando o palpite do jogador
@@ -8,38 +7,38 @@ let guess = document.getElementById('inputBox');
 
 //pegando o numero de tentativas do jogador
 let attempts = document.getElementById('attempts');
-
 let attemptsNumber = 0;
-
 let attemptsList = [];
-
 let textAnaliser = document.getElementById("textOutput");
 
 //pegando os palpites do jogador
 let guesses = document.getElementById('guesses');
 
-// guess.onkeydown = rodaCoisaQuandoTeclar;
-
+// guess.onkeydown = playGame;
 guess.addEventListener('keydown', playGame);
+
+newGameButton.onclick = resetOnNewGame;
 
 function playGame(e) {
     if (e.key === 'Enter') {
         // pega o que esta escrito
         const valorEscrito = e.target.value;
-        console.log(valorEscrito);
 
         // verificar se o numero digitado ja foi digitado. Se ja foi, nao incrementa tentativa nem adiciona na lista!!!!
-
-        newGameButton.onclick = resetOnNewGame;
-
+        if (attemptsList.includes(' ' + valorEscrito) == true){ 
+            e.target.value = null;
+            textAnaliser.innerHTML = "You have already used " + valorEscrito + " before. Try another guess"
+            return;
+        }
         attemptsNumber++;
         attempts.innerHTML = attemptsNumber;
-        attemptsList.push(valorEscrito);
+        attemptsList.push(' ' + valorEscrito);
         guesses.innerHTML = attemptsList;
 
         if (valorEscrito == randomNumber) {
             textAnaliser.innerHTML = "Congratulations!!!!!"
             e.target.disabled = true;
+            return;
         } else {
             if (valorEscrito > randomNumber) { //imprimindo se o palpite foi muito baixo ou muito alto
                 textAnaliser.innerHTML = "Your number is too high"
@@ -48,11 +47,14 @@ function playGame(e) {
             }
             e.target.value = null; // valor que a pessoa digitou some da caixinha para nao atrapalhar a escrita do proximo
         }
+
+        if(attemptsNumber == 10) {
+            textAnaliser.innerHTML = "You have used 10 attempts. The correct number was " + randomNumber + " . Try again!"
+            e.target.disabled = true;
+        }
     }
 }
 
-//function verifyGuessIsNew(number){
-    
 function resetOnNewGame() {
     attemptsNumber = 0;
     attemptsList = [];
